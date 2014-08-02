b2927d277d04c4a1f4f544d1841d825127a30a90
Dunya Kirkali <dunyakirkali@gmail.com>
2014-08-02 14:10:29 +0200

    removed coinbase init

--- config/initializers/coinbase.rb
+++
@@ -1,6 +0,0 @@
-user_credentials = {
-    :access_token => 'access_token',
-    :refresh_token => 'refresh_token',
-    :expires_at => Time.now + 1.day
-}
-coinbase = Coinbase::OAuthClient.new(ENV['COINBASE_CLIENT_ID'], ENV['COINBASE_CLIENT_SECRET'], user_credentials)
