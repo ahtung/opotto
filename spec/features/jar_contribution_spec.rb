bf02667327a8c2c9240677059127115ad7be9c3b
Dunya Kirkali <dunyakirkali@gmail.com>
2014-08-02 17:02:04 +0200

    more contribution tests

--- spec/features/jar_contribution_spec.rb
+++ spec/features/jar_contribution_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User' do
-  let!(:user) { FactoryGirl.create(:user, :with_jars) }
+  let!(:user) { FactoryGirl.create(:user, :with_jars, :with_contributions) }
   
   before :each do
     login user
@@ -9,6 +9,27 @@ describe 'User' do
   
   it 'should be able to contribute to his/her own jar' do
     jar = user.jars.first
+    visit jar_path(jar)
+    click_on 'Contribute'
+    fill_in 'contribution_amount', with: 1000
+    click_on 'Save'
+    
+    expect(page).to have_content('Contribution was successfully created.')
+  end
+  
+  it 'should be able to make multiple contributions to the same jar' do
+    jar = user.contributed_jars.first
+    visit jar_path(jar)
+    click_on 'Contribute'
+    fill_in 'contribution_amount', with: 1000
+    click_on 'Save'
+    
+    expect(page).to have_content('Contribution was successfully created.')
+  end
+  
+  it "should be able to contribute to someone else's own jar" do
+    another_user = FactoryGirl.create(:user)
+    jar = FactoryGirl.create(:jar, owner: another_user)
     visit jar_path(jar)
     click_on 'Contribute'
     fill_in 'contribution_amount', with: 1000
