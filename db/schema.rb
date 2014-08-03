12be25dc3a16f6f12e993f042d5cdbd8d370e371
Dunya Kirkali <dunyakirkali@gmail.com>
2014-08-03 19:29:22 +0200

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140802132607) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contributions", force: true do |t|
     t.integer  "user_id"
     t.integer  "jar_id"
@@ -43,7 +46,7 @@ ActiveRecord::Schema.define(version: 20140802132607) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
