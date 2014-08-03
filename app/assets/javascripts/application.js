208887d21a26d4fa64230c3b3ab7d763a9c1f168
Dunya Kirkali <dunyakirkali@gmail.com>
2014-08-03 12:54:25 +0200

    refactorings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,35 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require jquery.transit.min
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ 
+	$(document).foundation();
+	
+	var toggle = true;
+	
+	// $('#bottom').css({ rotateX: '90deg' });
+	
+	$('body').on('click', function() {
+		
+		var x = $('#top').outerWidth() / 2;
+		var y = $('#bottom').outerHeight();
+		$('#top').transition({
+			transformOrigin: x + 'px ' + '0px',
+		  perspective: '500px',
+		  rotateX: (toggle) ? '-90deg' : '0deg',
+			// height: (toggle) ? '0' : '45px'
+		}, 1000, function() {
+			toggle = !toggle;	
+		});
+		$('#bottom').transition({
+			transformOrigin: x + 'px ' + y + 'px',
+		  perspective: '500px',
+		  rotateX: (toggle) ? '0deg' : '90deg',
+			// height: (toggle) ? '45px' : '0'
+		}, 1000, function(){	
+    	// $(this).css('transform', '');
+  	});
+	});
+});
