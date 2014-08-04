05658771588d2ba78015afdee41a0ab6b692cdbd
Dunya Kirkali <dunyakirkali@gmail.com>
2014-08-04 23:27:10 +0200

    better views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
     @users_contributed_jars = current_user.contributed_jars
     @contributable_jars = Jar.not_by(current_user).not_contributed_by(current_user)
   end
+  
+  def welcome
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,14 @@
 Rails.application.routes.draw do
-  get 'contributions/new'
-
-  get 'contributions/create'
-
-  get 'contributions/update'
-
-  get 'contributions/edit'
-
-  get 'contributions/destroy'
-
-  get 'contributions/index'
-
-  get 'contributions/show'
-
-  root to: 'home#index'
-  get 'home/index'
-
+  
   devise_for :users
-
   resources :jars do
     resources :contributions
   end
+
+  get 'home/index'
+  get 'home/welcome'    
+  
+  root to: 'home#welcome'
+
 end
 
