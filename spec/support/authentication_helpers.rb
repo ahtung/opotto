a7fcdec78574a6d80cb3999581413e9a2fe64f36
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-09 21:54:57 +0100

    fixed broken tests

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -3,6 +3,6 @@ module AuthenticationHelpers
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
-    click_on 'Sign in'
+    click_on 'Log in'
   end
 end
