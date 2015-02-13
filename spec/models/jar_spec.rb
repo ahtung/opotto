731b74fe4b68dd201e13f8a0ca4f7de168d8fc2a
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-13 22:47:37 +0100

    pimp up specs and factoris

--- spec/models/jar_spec.rb
+++ spec/models/jar_spec.rb
@@ -4,4 +4,14 @@ describe Jar do
   it { should belong_to(:owner).class_name('User') }
   it { should have_many(:contributions).dependent(:destroy) }
   it { should have_many(:contributors).through(:contributions).class_name('User') }
+
+  it '#total_contribution' do
+    jar = create(:jar, :with_contributions)
+    expect(jar.total_contribution).to eq (jar.contributions.sum('amount') / 100).to_f
+  end
+
+  it '#total_contributors' do
+    jar = create(:jar, :with_contributors)
+    expect(jar.total_contributors).to eq jar.contributors.count
+  end
 end
