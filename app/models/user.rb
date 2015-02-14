ad713165558b1beef5ba5cf2ecf2a08d649b5e79
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-14 23:24:42 +0100

    new hash syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-  
+
   has_many :jars, dependent: :destroy, foreign_key: :owner_id
-  has_many :contributions, :dependent => :destroy
+  has_many :contributions, dependent: :destroy
   has_many :contributed_jars, -> { uniq }, through: :contributions, source: :jar
-  
+
   def uncontributed_jars
     jars - contributed_jars
   end
