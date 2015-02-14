90d6c123974ae284237d087da311968bc39d9779
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-15 00:35:42 +0100

    lean up

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,6 +1,6 @@
 # ContributionsController
 class ContributionsController < ApplicationController
-  before_action :set_jar, only: [:new, :show, :edit, :update, :destroy]
+  before_action :set_jar, only: :new
 
   # GET /contributions/new
   def new
@@ -11,24 +11,22 @@ class ContributionsController < ApplicationController
   def create
     @contribution = current_user.contributions.build(contribution_params)
 
-    respond_to do |format|
-      if @contribution.save
-        format.html { redirect_to root_path, notice: 'Contribution was successfully created.' }
-        format.json { render :show, status: :created, location: @contribution }
-      else
-        format.html { render :new }
-        format.json { render json: @contribution.errors, status: :unprocessable_entity }
-      end
+    if @contribution.save
+      redirect_to root_path, notice: 'Contribution was successfully created.'
+    else
+      format.html { render :new }
+      format.json { render json: @contribution.errors, status: :unprocessable_entity }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_jar
-      @jar = Jar.find(params[:jar_id])
-    end
 
-    def contribution_params
-      params.require(:contribution).permit(:jar_id, :amount)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_jar
+    @jar = Jar.find(params[:jar_id])
+  end
+
+  def contribution_params
+    params.require(:contribution).permit(:jar_id, :amount)
+  end
 end
