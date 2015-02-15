92bba4746634abfe404108c634a19d09f6acf0e0
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-15 22:21:54 +0100

    clean up

--- app/workers/friend_sync_worker_worker.rb
+++ app/workers/friend_sync_worker_worker.rb
@@ -2,11 +2,11 @@ class FriendSyncWorkerWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-    user_id = User.find(user_id)
+    user = User.find(user_id)
     google_contacts_user = GoogleContactsApi::User.new(access_token)
     google_contacts_user.contacts.each do |contact|
       ActiveRecord::Base.transaction do
-        contacts.where(email: contact.primary_email).first_or_create(name: contact.fullName, password: Devise.friendly_token[0, 20])
+        user.contacts.where(email: contact.primary_email).first_or_create(name: contact.fullName, password: Devise.friendly_token[0, 20])
       end
     end
   end
