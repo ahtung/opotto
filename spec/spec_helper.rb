5c3f75e935695359e8303378793134f52c6b82ee
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-02-15 13:45:49 +0200

    added user model test and helper for oauth2  authentication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,12 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
