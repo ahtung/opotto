115808a205bc82026138b74494a9e89f51a117d3
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-02-15 18:31:16 +0200

    boyscouting

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,34 +31,36 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
-      user = User.where(:email => data["email"]).first
+      user = User.where(:email => data['email']).first
 
       unless user
-          user = User.create(name: data["name"],
-             email: data["email"],
-             password: Devise.friendly_token[0,20],
+          user = User.create(name: data['name'],
+             email: data['email'],
+             password: Devise.friendly_token[0, 20],
              refresh_token: access_token.credentials.refresh_token
           )
       end
       user
   end
 
-  #private
+  private
 
+  # Imports user's contact list after it is created
   def import_contacts
     return unless access_token
     google_contacts_user = GoogleContactsApi::User.new(access_token)
     google_contacts_user.contacts.each do |contact|
-      contacts.where(email: contact.primary_email).first_or_create(name: contact.fullName, password: Devise.friendly_token[0,20])
+      contacts.where(email: contact.primary_email).first_or_create(name: contact.fullName, password: Devise.friendly_token[0, 20])
     end
   end
 
+  # Gets the access_token using users's refresh token
   def access_token
     return unless refresh_token
-    client = OAuth2::Client.new ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],{
-       :site => 'https://accounts.google.com',
-       :authorize_url => "/o/oauth2/auth",
-       :token_url => "/o/oauth2/token",
+    client = OAuth2::Client.new ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+       site: 'https://accounts.google.com',
+       authorize_url: '/o/oauth2/auth',
+       token_url: '/o/oauth2/token'
      }
      OAuth2::AccessToken.from_hash(client, refresh_token: refresh_token).refresh!
   end
