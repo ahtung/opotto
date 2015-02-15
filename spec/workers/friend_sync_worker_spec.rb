f713a1fcd3fc912894738ab69c066677faba97bc
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-15 22:59:05 +0100

    correcting test

--- spec/workers/friend_sync_worker_spec.rb
+++ spec/workers/friend_sync_worker_spec.rb
@@ -1,4 +1,4 @@
 require 'rails_helper'
-RSpec.describe FriendSyncWorkerWorker, type: :worker do
+RSpec.describe FriendSyncWorker, type: :worker do
     pending "add some examples to (or delete) #{__FILE__}"
 end
