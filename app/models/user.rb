0d0befdb288d6bb7b20e2d87e223d5d115ea0374
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-02-15 18:36:12 +0200

    boyscouting

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,11 @@ class User < ActiveRecord::Base
     jars - contributed_jars
   end
 
+  # A method nedeed by omniauth-google-oauth2 gem
+  # User is being created if it does not exist
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
-      user = User.where(:email => data['email']).first
+      user = User.where(email: data['email']).first
 
       unless user
           user = User.create(name: data['name'],
