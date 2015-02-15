192d15f78303c053e03bb729b61e35bc6fd683e2
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-15 01:49:25 +0100

    some styling at least

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@
 class HomeController < ApplicationController
   # GET /
   def index
-    @users_jars = current_user.jars
-    @users_contributed_jars = current_user.contributed_jars
-    @contributable_jars = Jar.all
+    @jars = Jar.all
   end
 
   # GET /welcome
