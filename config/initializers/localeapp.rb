f54b74dd56eff03b5dfd7dcc6da613860179bc38
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-22 15:34:09 +0100

    add locales api key to env

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,5 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.api_key = '1KeDWgwBcSlGubRBxlKe15X8nLkW1ohECEourfB401UXb6VALN'
+  config.api_key = ENV['LOCALEAPP_API_KEY']
 end
