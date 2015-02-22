c9613283fc4b4b1dd723185d8e5cdce29ec29d4f
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-22 13:58:57 +0100

    add some documentation

--- app/policies/jar_policy.rb
+++ app/policies/jar_policy.rb
@@ -1,15 +1,19 @@
+# JarPolicy
 class JarPolicy
   attr_reader :user, :post
 
+  # init policy
   def initialize(user, jar)
     @user = user
     @jar = jar
   end
 
+  # update?
   def update?
     jar.owner == user
   end
 
+  # contribute?
   def contribute?
     jar.guests.include?(user) && jar.open?
   end
