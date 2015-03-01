feb4addaaa4845d37d60b3ef5ec393cfd088bdf0
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-01 15:01:04 +0100

    refactor contributions controller

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,19 +1,20 @@
 # ContributionsController
 class ContributionsController < ApplicationController
+
   include ActionView::Helpers::NumberHelper
+
   before_action :authenticate_user!
   before_action :set_jar, only: [:new, :create]
+  before_action :authorize_jar
   after_action :verify_authorized
 
   # GET /contributions/new
   def new
-    authorize @jar, :contribute?
     @contribution = @jar.contributions.build
   end
 
   # POST /contributions
   def create
-    authorize @jar, :contribute?
     @contribution = current_user.contributions.build(contribution_params)
 
     if @contribution.save
@@ -25,11 +26,17 @@ class ContributionsController < ApplicationController
 
   private
 
+  # authorize user from contributing to a jar
+  def authorize_jar
+    authorize @jar, :contribute?
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_jar
     @jar = Jar.find(params[:jar_id])
   end
 
+  # Strong params
   def contribution_params
     params.require(:contribution).permit(:jar_id, :amount)
   end
