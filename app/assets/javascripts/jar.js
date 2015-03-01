22c8eda33fd099111de6f0da5a6bc2c751ecb12d
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-01 16:53:34 +0100

    being able to mask

--- app/assets/javascripts/jar.js
+++ app/assets/javascripts/jar.js
@@ -14,9 +14,29 @@ var svgContainer = d3.select("#d3")
                      .attr("width", 200)
                      .attr("height", 200);
 
-//The line SVG Path we draw
-var lineGraph = svgContainer.append("path")
-                            .attr("d", lineFunction(lineData))
-                            .attr("stroke", "blue")
-                            .attr("stroke-width", 2)
-                            .attr("fill", "lime");
+var height = 200;
+var width = 200;
+var fullness = 1;
+
+svgContainer
+.append('defs')
+  .append('mask')
+    .attr('id', 'mask')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', width)
+    .attr('height', height)
+  .append("path")
+    .attr("d", lineFunction(lineData))
+    .attr("stroke", "blue")
+    .attr("stroke-width", 2)
+    .attr("fill", "lime");
+
+svgContainer
+.append("rect")
+  .attr('x', 0)
+  .attr('y', height - (fullness * height))
+  .attr('width', width)
+  .attr('height', (fullness * height))
+  .attr('fill', 'red')
+  .attr('mask', 'url(#mask)');
