756ab21592065072bc7b9834cac96597a21ed846
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-01 19:18:27 +0200

    modified d3 functions foreach .d3 element

--- app/assets/javascripts/jar.js
+++ app/assets/javascripts/jar.js
@@ -1,51 +1,56 @@
-//The data for our line
-var lineData = $(".d3").data('points')
+$(".d3").each(function(){
+  //The data for our line
+  var lineData = $(this).data('points');
 
-//This is the accessor function we talked about above
-var lineFunction = d3.svg.line()
-                         .x(function(d) { return d.x; })
-                         .y(function(d) { return d.y; })
-                         .interpolate("basis-closed");
-
-//The SVG Container
-var svgContainer = d3.select(".d3")
+  //This is the accessor function we talked about above
+  var lineFunction = d3.svg.line()
+                     .x(function(d) { return d.x; })
+                     .y(function(d) { return d.y; })
+                      .interpolate("basis-closed");
+  //The SVG Container
+  var svgContainer = d3.select(this)
                      .attr("width", 200)
                      .attr("height", 200);
 
-var height = 200;
-var width = 200;
-var fullness = $(".d3").data('fullness');
+  //The line SV.pushath we draw
+  var lineGraph = svgContainer.append("path")
+                     .attr("d", lineFunction(lineData))
+                     .attr("stroke", "blue")
+                     .attr("stroke-width", 2)
+                     .attr("fill", "lime");
 
-console.log(fullness);
-console.log(lineData);
+  var height = 200;
+  var width = 200;
+  var fullness = parseFloat($(this).data('fullness'));
 
-svgContainer
-.append('defs')
-  .append('mask')
-    .attr('id', 'mask')
+  svgContainer
+  .append('defs')
+    .append('mask')
+      .attr('id', 'mask')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', width)
+      .attr('height', height)
+    .append("path")
+      .attr("d", lineFunction(lineData))
+      .attr("stroke", "red")
+      .attr("stroke-width", 2)
+      .attr("fill", "lime");
+
+  svgContainer
+  .append("rect")
     .attr('x', 0)
     .attr('y', 0)
     .attr('width', width)
     .attr('height', height)
-  .append("path")
-    .attr("d", lineFunction(lineData))
-    .attr("stroke", "blue")
-    .attr("stroke-width", 2)
-    .attr("fill", "lime");
+    .attr('fill', 'blue');
 
-svgContainer
-.append("rect")
-  .attr('x', 0)
-  .attr('y', 0)
-  .attr('width', width)
-  .attr('height', height)
-  .attr('fill', 'blue');
-
-svgContainer
-.append("rect")
-  .attr('x', 0)
-  .attr('y', height - (fullness * height))
-  .attr('width', width)
-  .attr('height', (fullness * height))
-  .attr('fill', 'red')
-  .attr('mask', 'url(#mask)');
+  svgContainer
+  .append("rect")
+    .attr('x', 0)
+    .attr('y', height - (fullness * height))
+    .attr('width', width)
+    .attr('height', (fullness * height))
+    .attr('fill', 'red')
+    .attr('mask', 'url(#mask)');
+});
