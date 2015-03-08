22ddf02b8c798ee9da003fbc14fe5cc1a748fe1f
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-08 20:55:35 +0100

    efactorings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, only: :index
   # GET /
   def index
-    @jars = current_user.discoverable_jars
   end
 
   # GET /welcome
