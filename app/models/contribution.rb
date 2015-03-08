7d39ee7eb609ac4c9764a35c550f3b6913186448
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-08 22:29:02 +0200

    added upper_bound per pot validation to contribute model, needs to be improved

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,10 +3,9 @@ class Contribution < ActiveRecord::Base
   belongs_to :user
   belongs_to :jar
 
-  monetize :amount_cents, numericality: {
-    greater_than_or_equal_to: 1,
-    less_than_or_equal_to: 10_000
-  }
+  validate :amount_inside_the_pot_bounds
+
+  monetize :amount_cents
 
   # Returns the proper user name
   def owner_name
@@ -18,4 +17,13 @@ class Contribution < ActiveRecord::Base
       user.name
     end
   end
+
+  private
+
+  def amount_inside_the_pot_bounds
+    return true unless jar.upper_bound
+    return true if jar.total_contribution + amount <= jar.upper_bound
+    raise "Unable to..."
+  end
+
 end
