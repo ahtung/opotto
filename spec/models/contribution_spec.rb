7141986ecdb8acbdea8f4135f276916c34f28b6a
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-08 20:09:24 +0200

    added owner name tests & edited factory

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -4,4 +4,21 @@ describe Contribution do
   it { should belong_to(:user) }
   it { should belong_to(:jar) }
   it { should monetize(:amount_cents) }
+
+  describe '#owner_name' do
+    it 'should return N/A if contribution is anonymous' do
+      contribution = FactoryGirl.create(:contribution, :anonymous)
+      expect(contribution.owner_name).to eq('N/A')
+    end
+
+    it 'should return user email if it didn\'t set' do
+      contribution = FactoryGirl.create(:contribution, :with_user_noname)
+      expect(contribution.owner_name).to eq(contribution.user.email)
+    end
+
+    it 'should return user full_name if set' do
+      contribution = FactoryGirl.create(:contribution, :with_user_with_name)
+      expect(contribution.owner_name).to eq(contribution.user.name)
+    end
+  end
 end
