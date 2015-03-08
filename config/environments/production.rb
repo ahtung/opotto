2ba75eacf70ef616f406400f076af91f19d57fa1
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-09 00:36:43 +0100

    enforce ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+
+  config.force_ssl = true
 end
