e99f08efc94f05a209123563c78feecc205ebfad
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-15 15:01:04 +0100

    refactorings

--- spec/models/jar_spec.rb
+++ spec/models/jar_spec.rb
@@ -39,87 +39,88 @@ describe Jar, focus: true do
   end
 
   # Instenace methods
-  describe '#fullness' do
-    it 'should return 0 if no contributions' do
-      jar = create(:jar)
-      expect(jar.fullness).to eq 0
-    end
-
-    it 'should return x if no contributions' do
-      jar = create(:jar, :with_contributions)
-      expect(jar.fullness).to eq jar.contributions.map(&:amount).inject { |sum, x| sum + x } / 1000
-    end
-  end
-
-  describe '#total_contribution' do
-    it 'should return 0 if no contributions' do
-      jar = create(:jar)
-      expect(jar.total_contribution).to eq 0
-    end
-
-    it 'should return the sum of contributions if contributions' do
-      jar = create(:jar, :with_contributions)
-      expect(jar.total_contribution).to eq jar.contributions.map(&:amount).inject { |sum, x| sum + x }
-    end
-  end
-
-  describe '#total_contributors' do
-    it 'should return 0 if no contributors' do
-      jar = create(:jar)
-      expect(jar.total_contributors).to eq 0
-    end
-
-    it 'should return the number of contributors if contributors' do
-      jar = create(:jar, :with_contributions)
-      expect(jar.total_contributors).to eq jar.contributors.count
-    end
-  end
-
-  describe '#total_guests' do
-    it 'should return 0 if no guests' do
-      jar = create(:jar)
-      expect(jar.total_guests).to eq 0
-    end
-
-    it 'should return the number of guests if guests' do
-      jar = create(:jar, :with_guests)
-      expect(jar.total_guests).to eq jar.guests.count
-    end
-  end
-
-  describe '.open' do
-    it 'should return only open pots' do
-      create_list(:jar, 2, :open)
-      closed_jars = create_list(:jar, 2, :closed)
-      expect(Jar.open).not_to include closed_jars
-    end
-  end
-
-  describe '.closed' do
-    it 'should return only closed pots' do
-      open_jars = create_list(:jar, 2, :open)
-      create_list(:jar, 2, :closed)
-      expect(Jar.closed).not_to include open_jars
-    end
-  end
-
-  describe '#pot_points' do
-
-    before :each do
-      @jar = create(:jar, :with_contributions)
-    end
-
-    it 'should return a array of length 12' do
-      expect(@jar.pot_points.count).to eq(12)
-    end
-
-    describe 'should return a array of where item is a hash' do
-      let(:points) { @jar.pot_points }
-      it 'with key x' do
-        expect(points.first.has_key?(:x)).to be(true)
+  describe '#' do
+    describe 'fullness' do
+      it 'should return 0 if no contributions' do
+        jar = create(:jar)
+        expect(jar.fullness).to eq 0
       end
-      it 'with key y' do
-        expect(points.first.has_key?(:y)).to be(true)
+
+      it 'should return x if no contributions' do
+        jar = create(:jar, :with_contributions)
+        expect(jar.fullness).to eq jar.contributions.map(&:amount).inject { |sum, x| sum + x } / 1000
+      end
+    end
+
+    describe 'total_contribution' do
+      it 'should return 0 if no contributions' do
+        jar = create(:jar)
+        expect(jar.total_contribution).to eq 0
+      end
+
+      it 'should return the sum of contributions if contributions' do
+        jar = create(:jar, :with_contributions)
+        expect(jar.total_contribution).to eq jar.contributions.map(&:amount).inject { |sum, x| sum + x }
+      end
+    end
+
+    describe 'total_contributors' do
+      it 'should return 0 if no contributors' do
+        jar = create(:jar)
+        expect(jar.total_contributors).to eq 0
+      end
+
+      it 'should return the number of contributors if contributors' do
+        jar = create(:jar, :with_contributions)
+        expect(jar.total_contributors).to eq jar.contributors.count
+      end
+    end
+
+    describe 'total_guests' do
+      it 'should return 0 if no guests' do
+        jar = create(:jar)
+        expect(jar.total_guests).to eq 0
+      end
+
+      it 'should return the number of guests if guests' do
+        jar = create(:jar, :with_guests)
+        expect(jar.total_guests).to eq jar.guests.count
+      end
+    end
+
+    describe 'pot_points' do
+      let(:jar) { create(:jar, :with_contributions) }
+
+      it 'should return a array of length 12' do
+        expect(jar.pot_points.count).to eq(12)
+      end
+
+      describe 'should return a array of where item is a hash' do
+        let(:points) { jar.pot_points }
+        it 'with key x' do
+          expect(points.first.has_key?(:x)).to be(true)
+        end
+        it 'with key y' do
+          expect(points.first.has_key?(:y)).to be(true)
+        end
+      end
+    end
+  end
+
+  describe '.' do
+    describe 'open' do
+      it 'should return only open pots' do
+        create_list(:jar, 2, :open)
+        closed_jars = create_list(:jar, 2, :closed)
+        expect(Jar.open).not_to include closed_jars
+      end
+    end
+
+    describe 'closed' do
+      it 'should return only closed pots' do
+        open_jars = create_list(:jar, 2, :open)
+        create_list(:jar, 2, :closed)
+        expect(Jar.closed).not_to include open_jars
       end
     end
   end
