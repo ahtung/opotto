bc379bf0ec708509fb16fdbc0b61b09534ca7c0f
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-15 19:17:33 +0200

    added payments worker

---
+++ app/workers/payments_worker.rb
@@ -0,0 +1,12 @@
+# PaymentsWorker
+class PaymentsWorker
+  include Sidekiq::Worker
+
+  sidekiq_options retry: false
+
+  # perform worker
+  def perform(contribution_id)
+    contribution = Contribution.find(contribution_id)
+    contribution.complete_preapproval
+  end
+end
