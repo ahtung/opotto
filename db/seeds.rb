a336b893dd591c5e793be4540a5d5ced2170d855
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-15 13:57:12 +0100

    cleaning up factories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-FactoryGirl.create(:user, :with_jars, :with_contributions, email: 'dunyakirkali@gmail.com')
-FactoryGirl.create_list(:user, 3, :with_jars, :with_contributions)
+FactoryGirl.create(:user, :with_jars, email: 'dunyakirkali@gmail.com')
+FactoryGirl.create_list(:user, 3, :with_jars)
