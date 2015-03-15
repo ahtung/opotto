a3258a4a1258c987624c5792b00a417da5185794
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-15 23:44:39 +0200

    added timezone scripts to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # ApplicationController
 class ApplicationController < ActionController::Base
+  around_filter :set_time_zone
   protect_from_forgery
   include Pundit
 
@@ -12,5 +13,17 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = I18n.t('pundit.user_not_authorized')
     redirect_to(request.referrer || root_path)
+  end
+
+  def set_time_zone
+    old_time_zone = Time.zone
+    Time.zone = browser_timezone if browser_timezone.present?
+    yield
+  ensure
+    Time.zone = old_time_zone
+  end
+
+  def browser_timezone
+    cookies["browser.timezone"]
   end
 end
