3c110deaba13578533760fc114394ed283c013ec
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-28 23:18:35 +0100

    style

--- spec/workers/friend_sync_worker_spec.rb
+++ spec/workers/friend_sync_worker_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FriendSyncWorker, type: :worker do
-
   it { expect(FriendSyncWorker).to be_processed_in :default }
   it { expect(FriendSyncWorker).to be_retryable false }
 
