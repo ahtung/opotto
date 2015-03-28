97d2dff9c1bb9df0f085fa24f3bea501c8869d81
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-28 18:59:50 +0100

    clean up

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,7 +4,6 @@ class Contribution < ActiveRecord::Base
   belongs_to :jar
 
   validate :amount_inside_the_pot_bounds
-  # validate :preapproval_initiated, unless: 'Rails.env.test?'
 
   after_commit :initiate_payment, on: :create
   attr_accessor :authorization_url
@@ -25,40 +24,56 @@ class Contribution < ActiveRecord::Base
     end
   end
 
+  # Completes payment (pay secondary receiver)
   def complete_payment
-    api.execute :ExecutePayment, secondary_payment_options
-    Rails.logger.info("Payment log |  Payment Executed for #{secondary_payment_options}")
+    api.execute :ExecutePayment, secondary_payment_options do |response|
+      if response.success?
+        update_column(:status, 'completed')
+        Rails.logger.info "Payment log |  Payment completed for #{secondary_payment_options}"
+      else
+        update_column(:status, 'failed')
+        Rails.logger.error "Payment log |  Payment completed for #{secondary_payment_options}"
+      end
+    end
   end
 
+  # Refunds payment
   def refund_payment
-
   end
 
   private
 
   # Initiates a payment
   def initiate_payment
-    payment = api.execute :Pay, payment_options
-    update_payment_details(payment)
-    get_payment_info
-    PaymentsWorker.perform_in((jar.end_at - Time.zone.now), id)
+    api.execute :Pay, payment_options do |response|
+      if response.success?
+        update_payment_details(response)
+        get_payment_info
+        PaymentsWorker.perform_in(payment_time, id)
+        Rails.logger.info "Payment log |  Payment initiated for #{payment_options}"
+      else
+        Rails.logger.error "Payment log |  Payment initiated for #{payment_options}"
+      end
+    end
   end
 
   def update_payment_details(payment)
     self.authorization_url = api.payment_url(payment)
     update_column(:payment_key, payment.pay_key)
-    payment_time = (jar.end_at - Time.zone.now) / 60
-    Rails.logger.info("Payment log | Payment initiated with the payment key: #{payment.pay_key} in #{payment_time} minutes")
-    PaymentsWorker.perform_in(payment_time, id)
-    self.payment_key = payment.pay_key
+    Rails.logger.info("Payment log | Payment updated details with the payment key: #{payment.pay_key} in #{payment_time} minutes")
+  end
+
+  def payment_time
+    (jar.end_at - Time.zone.now) / 60
   end
 
   def get_payment_info
     api.execute(:PaymentDetails, pay_key: payment_key) do |response|
       if response.success?
         self.user = User.where(email: response.sender.email).first
+        Rails.logger.info "Payment log |  Payment got info #{response.sender.email}"
       else
-        Rails.logger.error "#{response.ack_code}: #{response.error_message}"
+        Rails.logger.error "Payment log |  Payment failed getting info #{response.ack_code}: #{response.error_message}"
       end
     end
   end
