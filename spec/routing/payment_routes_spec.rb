80b028a94aa8d43233b2f7838937b7ebffbf0279
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-28 22:53:36 +0100

    improved routing specs

---
+++ spec/routing/payment_routes_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "routing to payments", type: :routing do
+  it "does route payments/success tp payments#success" do
+    expect(get: "/payments/success").to route_to(
+      controller: "payments",
+      action: "success"
+    )
+  end
+
+  it "does route payments/failure tp payments#failure" do
+    expect(get: "/payments/failure").to route_to(
+      controller: "payments",
+      action: "failure"
+    )
+  end
+end
