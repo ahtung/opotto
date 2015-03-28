43f49a1604c07fff297863557ecaffc15f963eac
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-28 20:40:41 +0200

    worker perform schedule moved back to model

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -43,12 +43,12 @@ class Contribution < ActiveRecord::Base
   def refund_payment
   end
 
+  private
+
   # Sets the payment time for sidekiq
   def payment_time
     jar.end_at - Time.zone.now
   end
-
-  private
 
   # Initiates a payment
   def initiate_payment
@@ -56,6 +56,7 @@ class Contribution < ActiveRecord::Base
       if response.success?
         update_payment_details(response)
         get_payment_info
+        PaymentsWorker.perform_in(payment_time, id)
         Rails.logger.info "Payment log |  Payment initiated for #{payment_options}"
       else
         Rails.logger.error "Payment log |  Payment initiated for #{payment_options}"
