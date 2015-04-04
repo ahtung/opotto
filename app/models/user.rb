225845e991b15927169996bb98c25561225dc45a
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-05 00:02:37 +0200

    boyscouting

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ActiveRecord::Base
   has_many :invitations, dependent: :destroy
   has_many :invited_jars, -> { uniq }, through: :invitations, source: :jar
   has_many :friendships
-  has_many :friends, :through => :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
 
   after_commit :schedule_import_contacts
 
