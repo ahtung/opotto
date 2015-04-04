29e8a896c339327e6df537ec6b9f2581648e7804
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-04 23:52:33 +0200

    boyscouting

--- features/support/env.rb
+++ features/support/env.rb
@@ -62,12 +62,12 @@ Before('@omniauth') do
   OmniAuth.config.test_mode = true
   # Capybara.default_host = 'http://example.com'
 
-  OmniAuth.config.add_mock(:google_oauth2, {
+  OmniAuth.config.add_mock(:google_oauth2,
     uid:  '12345',
     info: {
       name: 'twitteruser',
     }
-  })
+  )
 end
 
 After('@omniauth') do
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,8 +7,7 @@ end
 Capybara.register_driver :poltergeist do |app|
     Capybara::Poltergeist::Driver.new(
       app,
-      window_size: [1280, 1024]#,
-      #debug:       true
+      window_size: [1280, 1024]
     )
   end
 Capybara.default_driver    = :poltergeist
