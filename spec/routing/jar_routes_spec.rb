248f2e47ed62154d42daa9552b09114c4ef2bb66
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-04 23:45:25 +0200

    boyscouting

--- spec/routing/jar_routes_spec.rb
+++ spec/routing/jar_routes_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "routing to jars", type: :routing do
-  it "does route jars/:id tp payments#show" do
-    expect(get: "/jars/1").to route_to(
-      controller: "jars",
-      action: "show",
+RSpec.describe 'routing to jars', type: :routing do
+  it 'does route jars/:id tp payments#show' do
+    expect(get: '/jars/1').to route_to(
+      controller: 'jars',
+      action: 'show',
       id: '1'
     )
   end
 
   it "does route jars/new tp payments#new" do
-    expect(get: "/jars/new").to route_to(
-      controller: "jars",
-      action: "new"
+    expect(get: '/jars/new').to route_to(
+      controller: 'jars',
+      action: 'new'
     )
   end
 end
