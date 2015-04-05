978df4768052756a52d16843fc1d8f9ed6a9c66b
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-05 16:39:57 +0200

    passing page features

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,14 +1,13 @@
 require 'capybara/poltergeist'
-# if ENV['RAILS_ENV'] == 'production'
-#   Capybara.app_host = 'http://www.opotto.com'
-#   Capybara.run_server = false
-# elsif ENV['RACK_ENV'] == 'staging'
-#   Capybara.app_host = 'http://opotto-staging.herokuapp.com'
-#   Capybara.run_server = false
-# else
-#   Capybara.app_host = 'http://localhost:3000'
-#   Capybara.run_server = true
-# end
+if ENV['RAILS_ENV'] == 'production'
+  Capybara.app_host = 'http://www.opotto.com'
+  Capybara.run_server = false
+elsif ENV['RAILS_ENV'] == 'staging'
+  Capybara.app_host = 'http://opotto-staging.herokuapp.com'
+  Capybara.run_server = false
+else
+  Capybara.run_server = true
+end
 
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
