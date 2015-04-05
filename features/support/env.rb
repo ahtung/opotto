4ecae3bfc69159f918812788af2b40d9ed2887bc
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-05 17:32:41 +0200

    fix login

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,11 +16,14 @@ Before('@omniauth_test') do
   OmniAuth.config.test_mode = true
 
   # the symbol passed to mock_auth is the same as the name of the provider set up in the initializer
-  OmniAuth.config.mock_auth[:google] = {
-      "provider" => "google",
-      "uid" => "http://xxxx.com/openid?id=118181138998978630963",
-      "info" => { "email"=>"test@xxxx.com", "first_name"=>"Test", "last_name"=>"User", "name"=>"Test User" }
-  }
+  OmniAuth.config.add_mock(:google_oauth2, {
+    uid: '12345',
+    refresh_token: '12345',
+    info: {
+      name: 'twitteruser',
+      email: "test@xxxx.com"
+    }
+  })
 end
 
 After('@omniauth_test') do
