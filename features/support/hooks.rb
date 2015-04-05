395f2c8bd530e59d2ad435f053849dbdc8a3747a
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-05 16:32:22 +0200

    disable remote cucumbe

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,15 +1,21 @@
 require 'capybara/poltergeist'
-if ENV['RACK_ENV'] == 'production'
-  Capybara.app_host = 'http://www.opotto.com'
-else
-  Capybara.app_host = 'http://opotto-staging.herokuapp.com'
-end
+# if ENV['RAILS_ENV'] == 'production'
+#   Capybara.app_host = 'http://www.opotto.com'
+#   Capybara.run_server = false
+# elsif ENV['RACK_ENV'] == 'staging'
+#   Capybara.app_host = 'http://opotto-staging.herokuapp.com'
+#   Capybara.run_server = false
+# else
+#   Capybara.app_host = 'http://localhost:3000'
+#   Capybara.run_server = true
+# end
+
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
     app,
     window_size: [1280, 1024]
   )
 end
+
 Capybara.default_driver    = :poltergeist
 Capybara.javascript_driver = :poltergeist
-Capybara.run_server = false
