324ee9b16b923ac8ca17b150177734573c70cadd
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-05-03 18:25:32 +0300

    analytics track code moved to production.rv & set to a env variable

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ Bundler.require(*Rails.groups)
 module Pot
   # Application
   class Application < Rails::Application
-    config.middleware.use Rack::GoogleAnalytics, tracker: 'UA-59771384-2'
     config.i18n.available_locales = [:tr, :en]
     config.generators.helper = false
     config.generators.stylesheets = false
