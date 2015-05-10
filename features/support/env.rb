e12313d8a9a7a0e672aa186febe6cba07f483cd0
Dunya Kirkali <dunyakirkali@gmail.com>
2015-05-10 18:27:20 +0200

    clean up

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,34 +22,24 @@ def select2(value, options)
   fail "Must pass a hash containing 'from'" if !options.is_a?(Hash) || !options.key?(:from)
   label = first('label', text: options[:from])
   fail "No label that contains '#{ label }' found" unless label
-  if options[:search]
-    # Open the search field
-    label.find(:xpath, '..').find('.select2-container .select2-search-field').click
-    # Enter a value
-    label.find(:xpath, '..').find('.select2-container .select2-search-field input').set value
-    # FIXME wait for the results from the server
-    sleep 1
-    # Click on the first match
-    find(:xpath, '//body').find('.select2-drop li').click
-  elsif options[:create]
-    # Open the search field
-    label.find(:xpath, '..').find('.select2-container a').click
-    # Enter a value
-    find(:xpath, '//body').find('.select2-drop-active.select2-with-searchbox input').set value
-    # FIXME wait for the results from the server
-    sleep 1
-    # Click on the first match
-    find(:xpath, '//body').find('.select2-drop li').click
-  elsif options[:multi]
-    # FIXME: Use capybara methods instead of JS
-    page.execute_script <<-JS
-      $('label:contains("#{ options[:from] }")').parent().find('input.select2-offscreen').select2('data', { text: '#{ value }' }, true);
-    JS
+  if options[:multi]
+    select2_multi(options[:from], value)
   else
-    container = label.find(:xpath, '..').find('.select2-container')
-    container.find('.select2-choice').click
-    find(:xpath, '//body').find('.select2-drop li', text: value).click
+    select2_normal(label, value)
   end
+end
+
+def select2_multi(from, value)
+  # FIXME: Use capybara methods instead of JS
+  page.execute_script <<-JS
+    $('label:contains("#{ from }")').parent().find('input.select2-offscreen').select2('data', { text: '#{ value }' }, true);
+  JS
+end
+
+def select2_normal(label, value)
+  container = label.find(:xpath, '..').find('.select2-container')
+  container.find('.select2-choice').click
+  find(:xpath, '//body').find('.select2-drop li', text: value).click
 end
 
 def fill_in_jar_form(jar)
