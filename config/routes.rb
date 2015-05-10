9ec9470c353ea41c91a9bfb532633a6d896efdb6
Dunya Kirkali <dunyakirkali@gmail.com>
2015-05-10 16:49:35 +0200

    made devise database_authenticatable added pass an email to seeds

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
 
   # User
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
+  # devise_scope :user do
+  #   delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  # end
   resources :users, only: :show
 
   # Discover
