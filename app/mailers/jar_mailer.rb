cdab52f3847010555e76b4e2f234590b945e2422
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-08-22 12:54:13 +0300

    added completed_email action to JarMailer

--- app/mailers/jar_mailer.rb
+++ app/mailers/jar_mailer.rb
@@ -1,4 +1,11 @@
 class JarMailer < ApplicationMailer
   default from: "from@example.com"
   layout 'mailer'
+
+  def completed_email(receiver, contribution)
+    @receiver = receiver
+    @contribution = contribution
+    @jar = contribution.jar
+    mail(subject: 'You have sucessfully contributed!', to: @receiver.email)
+  end
 end
