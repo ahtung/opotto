d517300397dd7fde68b9885784361cbb034980f7
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-22 16:33:17 +0200

    Boycouting

--- spec/features/guest/pages_spec.rb
+++ spec/features/guest/pages_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Guest', type: :feature, js: true do
-
   it 'should be able to browse about page' do
     visit page_path('about')
-    expect(page).to have_content t("site.about.content")
+    expect(page).to have_content t('site.about.content')
   end
 
   it 'should be able to browse contact page' do
     visit page_path('contact')
-    expect(page).to have_content t("site.contact.content")
+    expect(page).to have_content t('site.contact.content')
   end
 
   it 'should be able to browse contact page' do
     visit page_path('security')
-    expect(page).to have_content t("site.security.content")
+    expect(page).to have_content t('site.security.content')
   end
 end
