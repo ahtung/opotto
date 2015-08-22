5a70f618e32170faef5eeff98aa7e93b77491022
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-22 20:45:33 +0200

    boyscouting

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,11 @@
-class Rack::Attack
-  spammers = ENV['SPAMMERS'].split(/,\s*/)
-  spammer_regexp = Regexp.union(spammers)
-  blacklist("block referer spam") do |request|
-    request.referer =~ spammer_regexp
+# Rack
+module Rack
+  # Attack
+  class Attack
+    spammers = ENV['SPAMMERS'].split(/,\s*/)
+    spammer_regexp = Regexp.union(spammers)
+    blacklist('block referer spam') do |request|
+      request.referer =~ spammer_regexp
+    end
   end
 end
