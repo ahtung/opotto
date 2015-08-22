a0c9efa8b9575bbc5d16c8af761c273324ecd898
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-22 16:12:11 +0200

    refactoring spec

--- spec/features/user/contribute_spec.rb
+++ spec/features/user/contribute_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :feature, js: true do
+  let(:user) { create(:user, :with_paypal, password: '123QwETR') }
+  let(:jar) { create(:jar, guests: [user]) }
+  let(:contribution) { build(:contribution) }
+
   xit 'should be able to contribute to a jar' do
-    user = create(:user, :with_paypal, password: '123QwETR')
-    jar =  create(:jar, :public, guests: [user])
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
     click_on 'Log in'
+
     visit new_jar_contribution_path(jar)
-    fill_in 'contribution_amount', with: 50
+
+    fill_in 'contribution_amount', with: contribution.amount.to_f
     click_on t('jar.save')
+
     expect(page).to eq('YHES')
   end
 end
