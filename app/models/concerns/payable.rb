d32a50bdcdc2d4421c8eb4d5e0a4d019fa7f4db9
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-08-22 15:27:50 +0300

    success and payment worker is already moved to controller, removing from payable

--- app/models/concerns/payable.rb
+++ app/models/concerns/payable.rb
@@ -13,8 +13,6 @@ module Payable
     return unless response
     update_payment_details(response)
     payment_info
-    success!
-    PaymentsWorker.perform_in(payment_time, id)
   end
 
   # completes the paypal payment
