5a52fcd3de9b40734ae92725e4350800e83863a9
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-08-22 15:27:15 +0300

    added contribution methods on success and failure actions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,24 @@
 # PaymentsController
 class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_contribution, only: [:success, :failure]
+  after_action :verify_authorized
+
   # payment success
   def success
+    authorize @contribution
+    @contribution.success! if @contribution.initiated?
   end
 
   # payment failure
   def failure
+    authorize @contribution
+    @contribution.error! if @contribution.initiated?
+  end
+
+  private
+
+  def set_contribution
+    @contribution = Contribution.find(params[:contribution])
   end
 end
