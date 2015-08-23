1910273f9e2fca020f3beafc57f68302c42ed230
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-23 15:36:12 +0200

    Add b index specs

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
+  # Relations
   it { should belong_to(:jar) }
   it { should belong_to(:user) }
+
+  # DB
+  it { should have_db_index(:user_id) }
+  it { should have_db_index(:jar_id) }
 
   it '#invite_user' do
     invitation = create(:invitation)
