93403394ea95f7a79b6da4356c816dacbec222cd
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-08-29 13:23:51 +0300

    added faceboko link helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe ApplicationHelper, type: :helper do
       expect(helper.phone_number_link(phone_number)).to eq('<a href="tel://+90-312-284-31-48"><i class="material-icons">phone</i></a>')
     end
   end
+
+  describe '#facebook_link' do
+    it 'should return facebook page url for a given path' do
+      page_path = 'pages/Ahtung/1640658482879986'
+      expect(helper.facebook_link(page_path)).to eq('<a class="facebook_link" target="_blank" href="https://www.facebook.com/pages/Ahtung/1640658482879986"></a>')
+    end
+  end
 end
