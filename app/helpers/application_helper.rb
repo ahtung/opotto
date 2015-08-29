e402ef8f0792ec3a28bef7ed7cbf65f5eb62a4f9
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-29 14:32:27 +0200

    boyscouting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@ module ApplicationHelper
   def phone_number_link(text)
     sets_of_numbers = text.scan(/[0-9]+/)
     number = "+#{sets_of_numbers.join('-')}"
-    number_text = "+#{sets_of_numbers.join(' ')}"
     link_to "tel://#{number}" do
       '<i class="material-icons">phone</i>'.html_safe
     end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "#phone_number_link" do
-    it "returns phone number link" do
+  describe '#phone_number_link' do
+    it 'returns phone number link' do
       phone_number = '90 312 284 31 48'
       expect(helper.phone_number_link(phone_number)).to eq('<a href="tel://+90-312-284-31-48"><i class="material-icons">phone</i></a>')
     end
@@ -11,7 +11,8 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe '#facebook_link' do
     it 'should return facebook page url for a given path' do
       page_path = 'pages/Ahtung/1640658482879986'
-      expect(helper.facebook_link(page_path)).to eq('<a class="facebook_link" target="_blank" href="https://www.facebook.com/pages/Ahtung/1640658482879986"></a>')
+      expect(helper.facebook_link(page_path))
+        .to eq('<a class="facebook_link" target="_blank" href="https://www.facebook.com/pages/Ahtung/1640658482879986"></a>')
     end
   end
 end
