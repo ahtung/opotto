9520a21ed89492a0b05fc41e2d088aa051a5c0be
Dunya Kirkali <dunyakirkali@gmail.com>
2015-08-29 14:26:23 +0200

    Fix specs

--- spec/features/guest/pages_spec.rb
+++ spec/features/guest/pages_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Guest', type: :feature, js: true do
   it 'should be able to browse about page' do
     visit page_path('about')
-    expect(page).to have_content t('site.about.content')
+    expect(page).to have_content t('pages.about.content')
   end
 
   it 'should be able to browse contact page' do
     visit page_path('contact')
-    expect(page).to have_content t('site.contact.content')
+    expect(page).to have_content t('pages.contact.content')
   end
 
   it 'should be able to browse contact page' do
     visit page_path('security')
-    expect(page).to have_content t('site.security.content')
+    expect(page).to have_content t('pages.security.content')
   end
 end
