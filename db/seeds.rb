409d96d1d6ff2c4a1d81f035df06e3638e1b0a1c
Dunya Kirkali <dunyakirkali@gmail.com>
2016-01-17 20:06:40 +0100

    Add admins to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ if Rails.env.development? || Rails.env.staging?
   Invitation.delete_all
   Contribution.delete_all
 
-  onur = FactoryGirl.create(:user, email: 'onurkucukkece@gmail.com', password: '123QwETR')
-  dunya = FactoryGirl.create(:user, email: 'dunyakirkali@gmail.com', password: '123QwETR')
+  onur = FactoryGirl.create(:user, :admin, email: 'onurkucukkece@gmail.com', password: '123QwETR')
+  dunya = FactoryGirl.create(:user, :admin, email: 'dunyakirkali@gmail.com', password: '123QwETR')
   us = FactoryGirl.create(:user, :with_paypal, password: '123QwETR')
 
   onur.friends << [dunya, us]
