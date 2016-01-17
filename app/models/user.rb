e81ff040d54193046754d42780d9edf79fc56824
Dunya Kirkali <dunyakirkali@gmail.com>
2016-01-17 18:30:45 +0100

    Import contacts in a transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,9 +65,11 @@ class User < ActiveRecord::Base
     return unless access_token
     google_contacts_user = GoogleContactsApi::User.new(access_token)
     contact_details = get_contact_details(google_contacts_user)
-    contact_details.each do |contact_detail|
-      friend = User.where(email: contact_detail[:email].downcase).first_or_create(contact_detail)
-      friends << friend unless friends.include?(friend)
+    ActiveRecord::Base.transaction do
+      contact_details.each do |contact_detail|
+        friend = User.where(email: contact_detail[:email].downcase).first_or_create(contact_detail)
+        friends << friend unless friends.include?(friend)
+      end
     end
     update_attribute(:last_contact_sync_at, Time.zone.now)
   end
