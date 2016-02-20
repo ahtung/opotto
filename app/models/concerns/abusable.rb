a708dac45f8bdda8f5868da7ea9e1cfdb7c83d92
Dunya Kirkali <dunyakirkali@gmail.com>
2016-02-20 11:26:36 +0100

    Implement abusable

--- app/models/concerns/abusable.rb
+++ app/models/concerns/abusable.rb
@@ -3,10 +3,18 @@ module Abusable
   extend ActiveSupport::Concern
 
   included do
+    has_many :reported_abuses, class_name: 'Abuse', as: :resource do
+      def confirmed
+        find(:all, conditions: ["confirmed = ?", true])
+      end
 
+      def pending
+        find(:all, conditions: ["confirmed = ?", false])
+      end
+    end
   end
 
-  private
-
-
+  def is_an_abuse?
+    !self.reported_abuses.confirmed.blank?
+  end
 end
