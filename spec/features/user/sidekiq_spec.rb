9a226b7c4fc5796f45face457166a31274cb3fc4
Dunya Kirkali <dunyakirkali@gmail.com>
2016-03-12 18:17:14 +0100

    Fix specs

--- spec/features/user/sidekiq_spec.rb
+++ spec/features/user/sidekiq_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe 'User', type: :feature, js: true do
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
-    click_on 'Log in'
+    click_on 'Sign in'
     visit '/sidekiq'
     expect(page).not_to have_content('Dashboard')
   end
 
-  xit 'should be able to access sidekiq page if admin' do
+  it 'should be able to access sidekiq page if admin' do
     visit new_user_session_path
     fill_in 'user_email', with: admin.email
     fill_in 'user_password', with: admin.password
-    click_on 'Log in'
+    click_on 'Sign in'
     visit sidekiq_web_path
     expect(page).to have_content('Dashboard')
   end
