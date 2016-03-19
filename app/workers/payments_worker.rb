aa9dc0e995a9177ed94bdbc794530e3c5eab956e
Onur Kucukkece <onurkucukkece@gmail.com>
2016-03-19 20:46:42 +0100

    worker method changed

--- app/workers/payments_worker.rb
+++ app/workers/payments_worker.rb
@@ -8,7 +8,7 @@ class PaymentsWorker
   # perform worker
   def perform(contribution_id)
     contribution = Contribution.find(contribution_id)
-    contribution.complete_payment
+    contribution.make_preapproved_payments
     Rails.logger.info("Payment log | Payment worker for #{contribution_id}")
   end
 end
