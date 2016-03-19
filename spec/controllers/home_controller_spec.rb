cafc8b1af65dd1259a2203875233be6e0e3be8f3
Dunya Kirkali <dunyakirkali@gmail.com>
2016-03-19 20:48:58 +0100

    Correct Cloudflare country header check

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController, type: :controller do
   describe 'GET index' do
     describe 'from the NL' do
       it 'renders the index template' do
-        @request.env['CF-IPCountry'] = 'NL'
+        @request.env['HTTP_CF_IPCOUNTRY'] = 'NL'
         get :index
         expect(response).to render_template('index')
       end
@@ -12,7 +12,7 @@ RSpec.describe HomeController, type: :controller do
 
     describe 'from JP' do
       it 'renders the index template' do
-        @request.env['CF-IPCountry'] = 'JP'
+        @request.env['HTTP_CF_IPCOUNTRY'] = 'JP'
         get :index
         expect(response).to redirect_to(page_path('unsupported'))
       end
