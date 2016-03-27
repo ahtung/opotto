5a964e8f3ac77dc6c95895a13901a1241cfc3053
Onur Kucukkece <onurkucukkece@gmail.com>
2016-03-27 14:18:33 +0300

    added payment key column to contributions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319214237) do
+ActiveRecord::Schema.define(version: 20160327082732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160319214237) do
     t.string   "state"
     t.string   "amount_currency", default: "USD", null: false
     t.integer  "amount_cents",    default: 0,     null: false
+    t.string   "payment_key"
   end
 
   add_index "contributions", ["jar_id"], name: "index_contributions_on_jar_id", using: :btree
