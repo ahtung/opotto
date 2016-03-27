71db94057d1385c9f69e02b4f05bb6c12916725c
Dunya Kirkali <dunyakirkali@gmail.com>
2016-03-27 16:18:31 +0200

    Better payment worker

--- app/workers/payments_worker.rb
+++ app/workers/payments_worker.rb
@@ -6,9 +6,8 @@ class PaymentsWorker
   sidekiq_options queue: 'payments'
 
   # perform worker
-  def perform(contribution_id)
-    contribution = Contribution.find(contribution_id)
-    contribution.complete_payment
-    Rails.logger.info("Payment log | Payment worker for #{contribution_id}")
+  def perform(contribution_ids)
+    contributions = Contribution.where(id: contribution_ids)
+    contributions.each(&:complete_payment)
   end
 end
--- lib/tasks/paypal.rake
+++ lib/tasks/paypal.rake
@@ -4,5 +4,10 @@ namespace :paypal do
     users_ids = User.where(paypal_member: nil).limit(100).pluck(:id)
     PayPalChecker.perform_async(users_ids)
   end
-end
 
+  desc 'Pays Users\' contributions'
+  task pay: :environment do
+    contributions_ids = Contribution.with_state(:scheduled).limit(100).pluck(:id)
+    PaymentsWorker.perform_async(contributions_ids)
+  end
+end
