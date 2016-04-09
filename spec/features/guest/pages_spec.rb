0c64256c3c9939528a43aae5825f5847bbd8d5cf
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-09 11:45:50 +0200

    Boyscout

--- spec/features/guest/pages_spec.rb
+++ spec/features/guest/pages_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Guest', type: :feature do
-  describe "should be able to browse" do
+  describe 'should be able to browse' do
     %w(about contact security).each do |current_page|
-      it "#{ current_page } page" do
+      it "#{current_page} page" do
         visit page_path(current_page)
-        expect(page).to have_content t("pages.#{ current_page }.content")
+        expect(page).to have_content t("pages.#{current_page}.content")
       end
     end
   end
