fe347e408c1dc0f6fae34bb1aca1f8ef78f3d7e1
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-10 16:50:29 +0200

    Correct specs

--- spec/features/user/report_abuse_spec.rb
+++ spec/features/user/report_abuse_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Guest', type: :feature do
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
-    click_on 'Log in'
+    click_on 'Sign in'
     visit pot_path(pot)
     click_on 'Report abuse'
     expect(page).to have_content(t('pot.reported'))
