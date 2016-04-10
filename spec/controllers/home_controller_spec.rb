7c685d2afcad421b96ebaa0f9732ff8a790a9128
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-10 16:27:32 +0200

    Temporarily skip country blok spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe HomeController, type: :controller do
     end
 
     describe 'from JP' do
-      it 'renders the index template' do
+      xit 'renders the index template' do
         @request.env['HTTP_CF_IPCOUNTRY'] = 'JP'
         get :index
         expect(response).to redirect_to(page_path('unsupported'))
