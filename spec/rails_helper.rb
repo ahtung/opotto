02e2a94da14f8fa5499fef5559f3c6732c0ffbe2
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-04-16 22:10:11 +0300

    included mock macro

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
   config.include ActionView::Helpers::NumberHelper
   config.include ActionView::Helpers::DateHelper
   config.include StateMachineRspec::Matchers
+  config.include OmniauthMacros
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
