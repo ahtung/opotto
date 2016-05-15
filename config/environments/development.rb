dafc1dfbd78c28a86588090a4edb251262e0a5dd
Dunya Kirkali <dunyakirkali@gmail.com>
2016-05-15 14:21:20 +0200

    Improve

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.after_initialize do
-    Bullet.enable = true
+    Bullet.enable = false
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
