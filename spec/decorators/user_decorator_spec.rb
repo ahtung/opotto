9a65567cc8283e2c6a96637c8c534192e1486940
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-05-22 14:40:06 +0300

    added handle decorator test

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 
-describe UserDecorator do
+RSpec.describe UserDecorator, type: :decorator do
+  describe 'handle' do
+    it 'returns name if user has first and last name' do
+      user = create(:user, first_name: 'Dunya', last_name: 'Kirkali').decorate
+      expect(user.handle).to eq(user.highlighted_name)
+    end
+
+    it 'returns name with seperator if user has first and last name' do
+      user = create(:user, first_name: 'Dunya', last_name: 'Kirkali').decorate
+      expect(user.handle('-')).to eq(user.highlighted_name('-'))
+    end
+
+    it 'returns name with seperator and icon if user has first and last name' do
+      user = create(:user, first_name: 'Dunya', last_name: 'Kirkali').decorate
+      expect(user.handle('-', 'mail outline')).to eq(user.highlighted_name('-', 'mail outline'))
+    end
+
+    it 'returns email if user has no name' do
+      user = create(:user, first_name: nil, last_name: nil).decorate
+      expect(user.handle).to eq(user.highlighted_email)
+    end
+
+    it 'returns email with icon if user has no name' do
+      user = create(:user, first_name: nil, last_name: nil).decorate
+      expect(user.handle(false, 'mail outline')).to eq(user.highlighted_email('mail outline'))
+    end
+  end
 end
