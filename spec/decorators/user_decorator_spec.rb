f8f92a083be654cc6ad5a400c082a6b46f1a0b6f
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-05-22 18:48:34 +0300

    adapted decorator text to hash params

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator, type: :decorator do
+  before :each do
+    @options = {
+      seperator: ' ',
+      icon: false,
+      color: 'green'
+    }
+  end
+
   describe 'highlighted_name' do
   end
 
@@ -9,25 +17,14 @@ RSpec.describe UserDecorator, type: :decorator do
 
     context 'with icon' do
       it 'should return email with icon' do
-        expect(user.highlighted_email(true)).to eq("<p><span class=\"true-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
+        @options[:icon] = 'mail outline'
+        expect(user.highlighted_email(@options)).to eq("<p><i class=\"icon mail outline\"></i><span class=\"green-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
       end
     end
 
     context 'without icon' do
       it 'should return email without icon' do
-        expect(user.highlighted_email(false)).to eq("<p><span class=\"false-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
-      end
-    end
-
-    context 'with color' do
-      it 'should return email without icon with color' do
-        expect(user.highlighted_email(false, 'yellow')).to eq("<p><i class=\"icon yellow\"></i><span class=\"false-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
-      end
-    end
-
-    context 'without color' do
-      it 'should return email without icon with green color' do
-        expect(user.highlighted_email(false)).to eq("<p><span class=\"false-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
+        expect(user.highlighted_email(@options)).to eq("<p><span class=\"green-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
       end
     end
   end
@@ -35,27 +32,31 @@ RSpec.describe UserDecorator, type: :decorator do
   describe 'handle' do
     it 'returns name if user has first and last name' do
       user = create(:user).decorate
-      expect(user.handle).to eq(user.highlighted_name(false, false, 'green'))
+      expect(user.handle).to eq(user.highlighted_name(@options))
     end
 
     it 'returns name with seperator if user has first and last name' do
       user = create(:user).decorate
-      expect(user.handle('-')).to eq(user.highlighted_name('-', false, 'green'))
+      @options[:seperator] = '-'
+      expect(user.handle('-')).to eq(user.highlighted_name(@options))
     end
 
     it 'returns name with seperator and icon if user has first and last name' do
       user = create(:user).decorate
-      expect(user.handle('-', 'mail outline')).to eq(user.highlighted_name('-', 'mail outline', 'green'))
+      @options[:seperator] = '-'
+      @options[:icon] = 'mail outline'
+      expect(user.handle('-', 'mail outline')).to eq(user.highlighted_name(@options))
     end
 
     it 'returns email if user has no name' do
       user = create(:user, first_name: nil, last_name: nil).decorate
-      expect(user.handle).to eq("<p><span class=\"green-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
+      expect(user.handle).to eq(user.highlighted_email(@options))
     end
 
     it 'returns email with icon if user has no name' do
       user = create(:user, first_name: nil, last_name: nil).decorate
-      expect(user.handle(false, 'mail outline')).to eq("<p><i class=\"icon mail outline\"></i><span class=\"green-text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
+      @options[:icon] = 'mail outline'
+      expect(user.handle(false, 'mail outline')).to eq(user.highlighted_email(@options))
     end
   end
 end
