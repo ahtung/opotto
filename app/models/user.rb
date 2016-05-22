50b56b1d160ba044041a03c054cc8808e742c106
Dunya Kirkali <dunyakirkali@gmail.com>
2016-05-22 14:48:54 +0200

    Add User#name

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\Z}
 
+  def name
+    [first_name, last_name].join(' ')
+  end
+
   def name?
     return true if first_name && last_name
     false
