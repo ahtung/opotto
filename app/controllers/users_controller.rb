c0479cdc94b2b48bc6cd5626df6430661632c7fe
Dunya Kirkali <dunyakirkali@gmail.com>
2016-05-22 20:49:50 +0200

    Better navigation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,35 @@ class UsersController < ApplicationController
   before_action :authorize_user
   after_action :verify_authorized
 
+  decorates_assigned :pots
+
   # GET /:id
   def show
+    if params[:by] == 'invited'
+      show_invitations
+    elsif params[:by] == 'contributed'
+      show_contributions
+    else
+      show_pots
+    end
   end
 
   private
+
+  def show_pots
+    @pots = current_user.pots
+    @title = 'My Pots'
+  end
+
+  def show_invitations
+    @pots = current_user.invited_pots
+    @title = 'My Invitations'
+  end
+
+  def show_contributions
+    @pots = current_user.contributed_pots
+    @title = 'My Contributions'
+  end
 
   def set_user
     @user = current_user.decorate
