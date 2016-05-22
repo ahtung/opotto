16918e574b6ab6eb64dcdf048f1843c9bbeef4a1
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-05-22 22:43:13 +0300

    colon to todo

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,7 @@ module Users
 
     private
 
-    # TODO Improve below
+    # TODO: Improve below
     def update_user
       if request.env['omniauth.auth'].credentials
         @user.update_attribute(:refresh_token, request.env['omniauth.auth'].credentials.refresh_token)
