b3dc0f35900f0841c770b257375758202f55b787
Dunya Kirkali <dunyakirkali@gmail.com>
2016-05-22 18:56:50 +0200

    Fix faulty color classes

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe UserDecorator, type: :decorator do
     let(:user) { create(:user).decorate }
 
     it 'should return user name without an icon' do
-      output = "<p><span class=\"green-text\">#{user.first_name.upcase}</span> <span class=\"grey-text\">#{user.last_name}</span></p>"
+      output = "<p><span class=\"green text\">#{user.first_name.upcase}</span> <span class=\"grey text\">#{user.last_name}</span></p>"
       expect(user.highlighted_name(@options)).to eq(output)
     end
 
     it 'should return user name with an icon' do
       @options[:icon] = 'mail outline'
-      output = '<p><i class="icon mail outline"></i><span class="green-text">'
-      output += "#{user.first_name.upcase}</span> <span class=\"grey-text\">#{user.last_name}</span></p>"
+      output = '<p><i class="icon mail outline"></i><span class="green text">'
+      output += "#{user.first_name.upcase}</span> <span class=\"grey text\">#{user.last_name}</span></p>"
       expect(user.highlighted_name(@options)).to eq(output)
     end
   end
@@ -41,7 +41,7 @@ RSpec.describe UserDecorator, type: :decorator do
     let(:user) { create(:user).decorate }
 
     it 'should only return decorated name & surname' do
-      output = "<span class=\"green-text\">#{user.first_name.upcase}</span> <span class=\"grey-text\">#{user.last_name}</span>"
+      output = "<span class=\"green text\">#{user.first_name.upcase}</span> <span class=\"grey text\">#{user.last_name}</span>"
       expect(user.decorated_name(' ', 'green')).to eq(output)
     end
   end
@@ -52,14 +52,14 @@ RSpec.describe UserDecorator, type: :decorator do
     context 'with icon' do
       it 'should return email with icon' do
         @options[:icon] = 'mail outline'
-        output = "<p><i class=\"icon mail outline\"></i><span class=\"green.text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>"
+        output = "<p><i class=\"icon mail outline\"></i><span class=\"green text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>"
         expect(user.highlighted_email(@options)).to eq(output)
       end
     end
 
     context 'without icon' do
       it 'should return email without icon' do
-        expect(user.highlighted_email(@options)).to eq("<p><span class=\"green.text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
+        expect(user.highlighted_email(@options)).to eq("<p><span class=\"green text\">#{user.email.match(/^(.*)@.*$/)[1]}</span></p>")
       end
     end
   end
