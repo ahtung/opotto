82ff3ea1203685285ab03ccd4f3443c33d07a6d2
onur <onurkucukkece@gmail.com>
2016-10-22 20:37:58 +0300

    security warning => protect_from_forgery with: :exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # ApplicationController
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
   include Pundit
 
   include HttpAcceptLanguage::AutoLocale
