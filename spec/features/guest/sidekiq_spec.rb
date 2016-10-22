90fb589536934ded40241c0c660f1e13eb2a3f80
Dunya Kirkali <dunyakirkali@gmail.com>
2016-10-22 20:18:46 +0200

    Increase coverage

--- spec/features/guest/sidekiq_spec.rb
+++ spec/features/guest/sidekiq_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Guest', type: :feature, js: true do
-  xit 'should not be able to access sidekiq page' do
+  it 'should not be able to access sidekiq page' do
     visit '/sidekiq'
     expect(page).not_to have_content('Dashboard')
   end
