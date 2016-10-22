fc17b2c4aff8ded6164f5f8f3727cf38f649b44e
onur <onurkucukkece@gmail.com>
2016-10-22 15:56:57 +0300

    deleted phone number helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 # Application Helper
 module ApplicationHelper
-  def phone_number_link(text)
-    sets_of_numbers = text.scan(/[0-9]+/)
-    number = "+#{sets_of_numbers.join('-')}"
-    link_to "tel://#{number}" do
-      content_tag :i, 'phone', class: 'material-icons'
-    end
-  end
-
   def facebook_link(page_path)
     link_to '', "https://www.facebook.com/#{page_path}", class: 'facebook_link', target: '_blank'
   end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe '#phone_number_link' do
-    it 'returns phone number link' do
-      phone_number = '90 312 284 31 48'
-      expect(helper.phone_number_link(phone_number))
-        .to eq('<a href="tel://+90-312-284-31-48"><i class="material-icons">phone</i></a>')
-    end
-  end
-
   describe '#facebook_link' do
     it 'should return facebook page url for a given path' do
       page_path = 'pages/Ahtung/1640658482879986'
