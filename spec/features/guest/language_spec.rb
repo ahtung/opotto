b986cea051e5fca76497de82d33762d6b63d8d90
onur <onurkucukkece@gmail.com>
2016-10-27 00:51:17 +0300

    added language specs

---
+++ spec/features/guest/language_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Guest', type: :feature, js: true do
+  it 'should be able to change language to Turkish' do
+    visit page_path('about')
+    within '.footer' do
+      find('.floating').click
+      find('a', text: 'Türkçe').click
+    end
+    expect(page).to have_content 'Hakkında'
+  end
+
+  it 'should be able to change language to English' do
+    visit page_path('security')
+    within '.footer' do
+      find('.floating').click
+      click_on 'English'
+    end
+    expect(page).to have_content 'Privacy'
+  end
+end
