c24377fd10226db0c2c1f9cef6a495f43498e3ec
onur <onurkucukkece@gmail.com>
2016-10-29 13:15:38 +0300

    edited user created pot spec but still xitted

--- spec/features/user/pot_spec.rb
+++ spec/features/user/pot_spec.rb
@@ -13,14 +13,22 @@ RSpec.describe 'User', type: :feature, js: true do
 
   xit 'should be able to create a pot' do
     visit root_path
-    click_on 'Get going'
+    # click_on 'Get going'
+    first('a.browse').click
+    click_on t('pages.about.title')
     fill_in 'pot_name', with: pot.name
 
-    first('.receiver-select .select-dropdown').click
-    first('.receiver-select .select-dropdown').first('li').click
+    # first('.receiver-select .select-dropdown').click
+    # first('.receiver-select .select-dropdown').first('li').click
+    find('#pot_receiver_id').find(:xpath, 'option[2]').select_option
 
-    first('.guests-select .select-dropdown').click
-    first('.guests-select .select-dropdown').first('li').click
+    # first('.guests-select .select-dropdown').click
+    # first('.guests-select .select-dropdown').first('li').click
+    find('input[name="search"]').set user.friends.first.name[0..3]
+
+    within '.menu.transition.visible' do
+      click_on user.friends.first.name
+    end
 
     fill_in 'pot_description', with: pot.description
     fill_in 'pot_end_at', with: Time.zone.now + 10.days
